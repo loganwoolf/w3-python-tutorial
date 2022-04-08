b6e99fbfc3bd130e31ed3cd03937227281631036
Logan Woolf <loganwoolf@gmail.com>
2022-04-08 16:19:59 -0600

    Add Strings content

---
+++ 03-strings.py
@@ -0,0 +1,17 @@
+# access specific index in string, zero indexed
+a = 'hello, world'
+print(a[1]) # >>> e
+
+# iterate through a string
+for x in "banana":
+  print(x)
+
+# get length of string
+print("String length is:", len("jjjj"))
+
+# check for presence/absence of string in another string
+print("free" in "freedom")
+print("free" not in "fredrickton")
+
+if "expensive" not in "wooly-mammoth":
+  print("No, expensive is NOT present")
