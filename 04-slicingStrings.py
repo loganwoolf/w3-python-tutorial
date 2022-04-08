ad201347e904bb47532a2c07dcb7babd55b6869f
Logan Woolf <loganwoolf@gmail.com>
2022-04-08 16:20:09 -0600

    Add slice content

---
+++ 04-slicingStrings.py
@@ -0,0 +1,12 @@
+a = "abcde12345"
+# gets positions 2, 3, and 4 "cde"
+print(a[2:5])
+
+# gets from start to before position 5
+print(a[:5])
+
+# gets from position 2 to end
+print(a[5:])
+
+# gets from fifth last to 
+print(a[-5:-1])
